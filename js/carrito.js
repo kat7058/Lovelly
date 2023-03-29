da52a5ff7f87b85e9e672312cfe5ec38d8d13023
kat7058 <katerinag17019@gmail.com>
2023-03-28 22:10:32 -0600

    finalizacion de una parte de las vistas

---
+++ js/carrito.js
@@ -0,0 +1,18 @@
+
+const carrito = document.getElementById('carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito');
+const vaciarCarrito = document.getElementById('vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+let arregloCarrito = [];
+
+/**Metodos */
+function cargarEventos(){
+/**Agregar curos
+ * eliminar curso
+ * vaciar el carrito del curso
+ */
+}
+
+
+
